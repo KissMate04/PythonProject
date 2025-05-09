2051838055be5b866be6da5b0745a73005d40e61
KissMate04 <km.kissmate@gmail.com>
2025-05-09 15:42:24 +0200

    setup file update...elvileg utoljára

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "pythonproject=pythonproject.main:main",
+            "pythonproject=pythonproject.launcher:main",
         ],
     },
     author="Kiss Máté",
